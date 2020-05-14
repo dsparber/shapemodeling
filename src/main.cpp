2b436a37770de0bd0bd3e3ca81e573cc72b1e555
Sebastian Winberg <winbergs@student.ethz.ch>
2020-05-14 12:37:45 +0200

    Integrated landmark into main executable with different pipeline modes

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,12 @@
 #include <imgui/imgui.h>
 
 /*** insert any necessary libigl headers here ***/
+#include <string>
+#include <memory>
 #include <math.h> 
+
 #include "PCA.h"
+#include "landmark.h"
 
 using namespace std;
 using namespace Eigen;
@@ -20,24 +24,54 @@ Eigen::MatrixXd V;
 // face array, #F x3
 Eigen::MatrixXi F;
 
+// Unique pointer to landmark manager
+std::unique_ptr<LandmarkManager> landmarkManager;
+
+// Pipeline mode for showing different window
+enum PipelineMode {
+	LANDMARK_MODE = 0,
+	FACE_ALIGNMENT_MODE,
+	PCA_MODE,
+	BONUS_MODE
+};
+
+enum PipelineMode pipelineMode = LANDMARK_MODE;
+
 bool exampleBool = false;
 int exampleInt = 2;
 
 
 // ************************Function Declaration ************************ //
 bool callback_mouse_move(Viewer &viewer, int mouse_x, int mouse_y);
-bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifiers);
-bool load_mesh(string filename);
+bool callback_mouse_down(Viewer &viewer, int button, int modifier);
+bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifier);
+bool callback_pre_draw(Viewer &viewer);
 bool callback_init(Viewer &viewer);
+bool load_mesh(const std::string &filename);
 // ******************************************************************** //
 
 
-bool callback_mouse_move(Viewer &viewer, int mouse_x, int mouse_y){
+bool callback_mouse_move(Viewer &viewer, int mouse_x, int mouse_y) {
+
 	viewer.mouse_mode = igl::opengl::glfw::Viewer::MouseMode::Translation;
+	
+	return false;
+
 }
 
+bool callback_mouse_down(Viewer &viewer, int button, int modifier) {
+	
+	if (pipelineMode == LANDMARK_MODE) {
+		// Passing on mouse down event to landmark manager
+		landmarkManager->callback_mouse_down(viewer, button, modifier);
+	}
 
-bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifiers){
+	return false;
+
+}
+
+bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifier) {
+	
 	switch (key) {
 		case '1':
 			break;
@@ -49,46 +83,105 @@ bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifiers){
 			break;
 	}
 
+	if (pipelineMode == LANDMARK_MODE) {
+		// Passing on key pressed event to landmark manager
+		landmarkManager->callback_key_pressed(viewer, key, modifier);
+	}
+
+	return false;
+
 }
 
-bool load_mesh(string filename){
-	igl::read_triangle_mesh(filename,V,F);
+bool callback_pre_draw(Viewer &viewer) {
+
+	//viewer.data().clear();
+	//viewer.data().set_mesh(V, F);
+
+	if (pipelineMode == LANDMARK_MODE) {
+		// Pre-drawing landmark points and labels
+		landmarkManager->callback_pre_draw(viewer);
+	} else {
+		// Removing all points and labels from viewer data
+		// NOTE: Hacky way, but this prevents having to call clean() and thus set_mesh() every time
+		viewer.data().set_points(Eigen::MatrixXd(0, 3), Eigen::MatrixXd(0, 3));
+		viewer.data().labels_positions.resize(0, 3);
+		viewer.data().labels_strings.clear();
+	}
+
+	return false; 
+
+}
+
+bool callback_init(Viewer &viewer) {
+
+	return false;
+
+}
+
+bool load_mesh(const std::string &filename) {
+
+	// Reading triangle from file (multiple mesh types possible)
+	igl::read_triangle_mesh(filename, V, F);
+
+	// Clearing viewer data, setting mesh and aligning camera
 	viewer.data().clear();
 	viewer.data().set_mesh(V, F);
 	viewer.core.align_camera_center(V);
 
+	// Initializing landmark manager
+	landmarkManager = std::unique_ptr<LandmarkManager>(new LandmarkManager(V, F, viewer));
+
 	return true;
+
 }
 
-bool callback_init(Viewer &viewer){
-	return false;
-}
+int main(int argc,char *argv[]) {
 
-int main(int argc,char *argv[]){
-	if(argc != 2){
-		cout << "Usage ./assignment6 <mesh.off/obj>" << endl;
+	if (argc != 2) {
+		std::cout << "Usage ./assignment6 <mesh.off/obj>" << std::endl;
 		load_mesh("../data/aligned_faces_example/example1/fabian-brille.objaligned.obj");
-	}else{
+	} else {
 		load_mesh(argv[1]);
 	}
 
 	igl::opengl::glfw::imgui::ImGuiMenu menu;
 	viewer.plugins.push_back(&menu);
 
-	menu.callback_draw_viewer_menu = [&]()
-	{
+	menu.callback_draw_viewer_menu = [&]() {
+
 		// Draw parent menu content
 		menu.draw_viewer_menu();
 
-		if (ImGui::CollapsingHeader("Parameters", ImGuiTreeNodeFlags_DefaultOpen))
-		{
+		// Useful to check ImGui stuff
+		//ImGui::ShowDemoWindow();
+
+		if (ImGui::CollapsingHeader("Parameters", ImGuiTreeNodeFlags_DefaultOpen)) {
+
+			const char* mode[] = {"Landmark", "Face Alignment", "PCA", "Bonus"};
+            int index = pipelineMode;
+            if (ImGui::Combo("Pipeline Mode", &index, mode, IM_ARRAYSIZE(mode))) {
+                // Updating pipeline mode
+                pipelineMode = static_cast<enum PipelineMode>(index);
+            }
+
 			ImGui::Checkbox("Example Boolean", &exampleBool);
 			ImGui::InputInt("Example Integer", &exampleInt);
+			
 		}
+
+		if (pipelineMode == LANDMARK_MODE) {
+			// Drawing custom landmark window menu
+			landmarkManager->callback_draw_viewer_menu();
+		}
+
 	};
 
-//   viewer.callback_key_pressed = callback_key_pressed;
-//   viewer.callback_mouse_move = callback_mouse_move;
-//   viewer.callback_init = callback_init;
+	viewer.callback_key_pressed = callback_key_pressed;
+	//viewer.callback_mouse_move = callback_mouse_move;
+	viewer.callback_mouse_down = callback_mouse_down;
+	viewer.callback_pre_draw = callback_pre_draw;
+	//viewer.callback_init = callback_init;
+
 	viewer.launch();
+
 }
